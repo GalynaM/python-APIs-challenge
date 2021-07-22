79f60b62733bb39c3579ddbca7d8dde1802c4d0a
GalynaM <gmalinskaya@gmail.com>
2021-07-22 14:27:28 -0700

    Calls to Weather APIs were added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-APIs-challenge
 Initial files added
+Random selection of over 500 cities is added
+Calls to OpenWeather APIs are added to retrieve weather selected cities 
--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e177161205c800990b4bee8d53c03467"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB_y5X3u0-y8B5RuI1TQx2YVT2LO1l6G4g"
