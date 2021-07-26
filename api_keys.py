275cd7cc8690411b0bb9eca2ade652c6c2d7ec0c
GalynaM <gmalinskaya@gmail.com>
2021-07-26 01:38:50 -0700

    Config file edited

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e177161205c800990b4bee8d53c03467"
+weather_api_key = "your_key"
 
 # Google API Key
-g_key = "AIzaSyB_y5X3u0-y8B5RuI1TQx2YVT2LO1l6G4g"
+g_key = "your_key"
